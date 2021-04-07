9272f3f3cd7edfec128affa5f045d1d9fdb788c2
Cameron Adams <camgadams@gmail.com>
2021-04-07 11:18:42 +0200

    fix: broken links

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,30 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
-    return (
-        <ul className="links">
-          <li>
-            <a href="/">HOME</a>
-          </li>
-          <li>
-            <a href="/">PROJECTS</a>
-          </li>
-          <li>
-            <a href="/">ABOUT</a>
-          </li>
-          <li>
-            <a href="/">CONTACT</a>
-          </li>
-          <li>
-            <a href="/">CV</a>
-          </li>
-        </ul>
-      );
+  return (
+    <ul className="links">
+      <li>
+        <a>HOME</a>
+      </li>
+      <li>
+        <a>PROJECTS</a>
+      </li>
+      <li>
+        <a>ABOUT</a>
+      </li>
+      <li>
+        <a>CONTACT</a>
+      </li>
+      <li>
+        <a>CV</a>
+      </li>
+    </ul>
+  );
 };
 
-export default NavBar
-
+export default NavBar;
 
 // const Header = () => {
-    
+
 //   };
